84a6fc51334a7194878caf37ee270f7b596504a6
ed.cannon <eoc210@googlemail.com>
2011-06-12 15:34:20 -0600

    getArticles() method now returns articles in a list object.

--- src/WebExtraction/Scraper.py
+++ src/WebExtraction/Scraper.py
@@ -13,9 +13,12 @@ class Scraper:
         uriFileHandler = open(ifile,'r')
         self.URIList = uriFileHandler.readlines()
     def getArticles(self):
+        self.articles =[]
         for i in range(len(self.URIList)):
             f = urllib.request.urlopen(str(self.URIList[i]))
             article = f.readlines()
+            self.articles.append(article)
             print(article)
+        return self.articles
 s = Scraper(sys.argv[1])
 s.getArticles()
