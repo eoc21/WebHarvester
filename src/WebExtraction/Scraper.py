f81509b7977caf07226c6fc0222c007701ffe89b
ed.cannon <eoc210@googlemail.com>
2011-06-12 15:04:43 -0600

    Created Scraper class. Accepts file with uris to be scraped.

--- src/WebExtraction/Scraper.py
+++ src/WebExtraction/Scraper.py
@@ -4,3 +4,12 @@ Created on Jun 12, 2011
 @author: eoc21
 '''
 import os, sys, urllib.request
+
+class Scraper:
+    '''
+    Scrapes html articles from online journals.
+    '''
+    def __init__(self,ifile):
+        uriFileHandler = open(ifile,'r')
+        self.URIList = uriFileHandler.readlines()
+    
