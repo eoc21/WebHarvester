da158b24f6f86819485593fda9f58b6d58dcc806
ed.cannon <eoc210@googlemail.com>
2011-06-18 19:40:59 -0600

    Added chebi dictionary and ChebiDictionary class for looking up a name.

---
+++ src/Tokenization/DictionaryLookup.py
@@ -0,0 +1,27 @@
+'''
+Created on Jun 12, 2011
+Identifies tokens through a lookup.
+@author: eoc21
+'''
+import os, sys
+class ChebiDictionary():
+    def __init__(self):
+        self.dictionaryEntries=[]
+    def initializeDictionary(self):
+        path = os.path.expanduser('../../ChebiNames.txt')
+        dictF = open(path,'r')
+        dictValues = dictF.readlines()
+        for x in dictValues: self.dictionaryEntries.append(str(x).strip())
+    def IsPresent(self,name):
+        if name in self.dictionaryEntries:
+            return True
+        else:
+            return False
+    def getDictionaryEntry(self,name):
+        if name in self.dictionaryEntries:
+            return name
+        else:
+            return "Not found!"
+        
+c = ChebiDictionary()
+c.initializeDictionary()
