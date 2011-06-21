2eaef6e215c48d18ad010f0214279f268dbd279c
ed.cannon <eoc210@googlemail.com>
2011-06-20 22:10:05 -0600

    Cleaned up code.

--- src/Tokenization/Tokenizer.py
+++ src/Tokenization/Tokenizer.py
@@ -1,18 +1,8 @@
 '''
 Created on Jun 12, 2011
-General module for tokenization, html clean up
-paragraph identification and sentence splitting.
+General module for tokenization / identification of 
+chemicals in text.
 @author: eoc21
 '''
 import os, sys, re
-class ParagraphTokenizer:
-    def __init__(self):
-        self.paragraphs =[]
-    def splitText2Paragraphs(self,articles):
-        for i in range(len(articles)):
-            print(articles[i])
-    
-class SentenceTokenizer:
-    def __init__(self):
-        pass
     
--- src/WebExtraction/HTMLCustomParser.py
+++ src/WebExtraction/HTMLCustomParser.py
@@ -5,10 +5,19 @@ Created on Jun 18, 2011
 '''
 from html.parser import HTMLParser
 class ChemicalHTMLParser(HTMLParser):
-    def __init__(self):
+    def __init__(self,articles):
         HTMLParser.__init__(self)
         self.paragraphs =[]
+        self.cleanArticles(articles)
     def handle_data(self,data):
         self.paragraphs.append(data)
     def getParagraphs(self):
         return self.paragraphs
+    def cleanArticles(self,articles):
+        partialClean = str(articles).replace('[', '')
+        cleaner = partialClean.replace(']','')
+        cleanerStill = cleaner.replace('b\'','')
+        cleaned = cleanerStill.replace('\\n\',','')
+        self.feed(cleaned)
+
+
